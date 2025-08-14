279cff0bae7f4958afe4e4b1cb71cba0e8c81b37
szoumoc <soumosaha4327@gmail.com>
2025-08-14 16:55:48 +0530

    persisting data with sql

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,19 @@ import (
 	"log"
 
 	"github.com/szoumoc/social/internal/env"
+	"github.com/szoumoc/social/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+	store := store.NewStorage(nil)
 	app := &application{
 		config: cfg,
+		store: store,
 	}
+
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/szoumoc/social
 
 go 1.24.5
 
-require github.com/go-chi/chi/v5 v5.2.2 
+require github.com/go-chi/chi/v5 v5.2.2
+
+require github.com/lib/pq v1.10.9 // indirect
