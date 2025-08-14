7b3d4b0cf766186c5bf2595d24a0b62d24823dcc
szoumoc <soumosaha4327@gmail.com>
2025-08-14 12:07:00 +0530

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/szoumoc/social/internal/env"
+)
+
+func main() {
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+	}
+	app := &application{
+		config: cfg,
+	}
+	mux := app.mount()
+	log.Fatal(app.run(mux))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/szoumoc/social
+
+go 1.24.5
+
+require github.com/go-chi/chi/v5 v5.2.2 
