14f3f83089ee132e7038922d7d3612dda9d89290
szoumoc <soumosaha4327@gmail.com>
2025-08-19 17:56:07 +0530

    feat: Pagination for feed

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,11 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/szoumoc/social/internal/store"
+)
 
 func (app *application) getFeedHandler(w http.ResponseWriter, r *http.Request) {
-	// Pagination, filters
+	// Pagination, filters, sorting
+	fq := store.PaginatedFeedQuery{
+		Limit:  10,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
 	ctx := r.Context()
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(324))
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(324), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
