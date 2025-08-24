7c044e2d336ddad1d01c0be279962bed9bd2f4ec
szoumoc <soumosaha4327@gmail.com>
2025-08-24 13:09:10 +0530

    fix: handle token password

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -151,7 +151,10 @@ func (app *application) createTokenHandler(w http.ResponseWriter, r *http.Reques
 		}
 		return
 	}
-
+	if err := user.Password.Compare(payload.Password); err != nil {
+		app.unauthorizedErrorResponse(w, r, err)
+		return
+	}
 	//	generate the token -> add claims
 	claims := jwt.MapClaims{
 		"sub": user.ID,
