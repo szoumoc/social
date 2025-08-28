bf95ae3d7ce1a9f7b4086c163e36168b4b957050
szoumoc <soumosaha4327@gmail.com>
2025-08-28 16:18:35 +0530

    feat: Graceful shutdown

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -26,11 +31,11 @@ type application struct {
 }
 
 type config struct {
-	addr        string
-	db          dbConfig
-	env         string
-	mail        mailConfig
-	apiURL      string
+	addr string
+	db   dbConfig
+	env  string
+	mail mailConfig
+	// apiURL      string
 	frontendURL string
 	auth        authConfig
 }
@@ -141,6 +146,33 @@ func (app *application) run(mux *chi.Mux) error {
 		ReadTimeout:  time.Second * 10,
 		IdleTimeout:  time.Minute,
 	}
+
+	shutdown := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		app.logger.Infow("signal caught", "signal", s.String())
+
+		shutdown <- srv.Shutdown(ctx)
+	}()
+
 	app.logger.Infow("Server has started", "addr", app.config.addr, "env", app.config.env)
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdown
+	if err != nil {
+		return err
+	}
+	app.logger.Infow("Server has stopped", "addr", app.config.addr, "env", app.config.env)
+	return nil
 }
